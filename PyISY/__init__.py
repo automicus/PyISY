8414e81736b4ce7a2ee3fe44b4273a47533e2931
Ryan Kraus <rmkraus@gmail.com>
2015-04-19 21:17:22 -0400

    Updated header of main __init__ file.

--- PyISY/__init__.py
+++ PyISY/__init__.py
@@ -1,16 +1,30 @@
 """
 PyISY - Python Library for the ISY Controller
 
-DESCRIPTION:
-	This module is a set of Python bindings for the ISY's REST API. The
-	ISY is developed by Universal Devices and is a home automation
-	controller for Insteon and X10 devices.
+This module is a set of Python bindings for the ISY's REST API. The
+ISY is developed by Universal Devices and is a home automation
+controller for Insteon and X10 devices.
 
-AUTHOR: Automicus (Ryan M. Kraus)
-COPYRIGHT: (C) 2014
-WRITTEN: December, 2014
+Copyright 2015 Ryan M. Kraus
+               automicus@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 from .ISY import ISY
 
 __version__ = '1.0.0'
+__author__ = 'Ryan M. Kraus'
+__email__ = 'automicus@gmail.com'
+__date__ = 'April 2015'
+__website__ = 'http://automic.us'
