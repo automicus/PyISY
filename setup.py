50ba2e67032992a997563e336bc4e4fe39af1e69
Ryan Kraus <rmkraus@gmail.com>
2015-04-21 22:36:06 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='PyISY',
     version='1.0.0',
     license='Apache License 2.0',
     url='http://automic.us/projects/pyisy',
+    download_url='https://github.com/automicus/pyisy/tarball/1.0.0',
     author='Ryan Kraus',
     author_email='automicus@gmail.com',
     description='Python module to talk to ISY994 from UDI.',
-    packages=find_packages(),
+    packages=['PyISY'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
     install_requires=['requests', 'VarEvents'],
+    keywords=['home automation', 'isy', 'isy994', 'isy-994', 'UDI'],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache License 2.0',
