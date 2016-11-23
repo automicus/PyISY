3f0f6211d779c1919de55466e127852b8170864c
Dan Taeyoung <provolot@gmail.com>
2016-11-23 15:23:36 -0500

    aux_properties should be a dict

--- PyISY/Nodes/node.py
+++ PyISY/Nodes/node.py
@@ -47,7 +47,7 @@ def parse_xml_properties(xmldoc):
     state_val = None
     state_uom = []
     state_prec = ''
-    aux_props = []
+    aux_props = {} 
     state_set = False
 
     props = xmldoc.getElementsByTagName('property')
@@ -71,12 +71,11 @@ def parse_xml_properties(xmldoc):
                 state_uom = units
                 state_prec = prec
             else:
-                aux_props.append({
-                    ATTR_ID: prop_id,
+                aux_props[prop_id] = {
                     ATTR_VALUE: val,
                     ATTR_PREC: prec,
                     ATTR_UOM: units
-                })
+                }
 
     return state_val, state_uom, state_prec, aux_props
 
