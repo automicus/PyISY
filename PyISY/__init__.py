ff11c810d4ca874e3d0ea688d09707ef6997e1ed
Ryan Kraus <rmkraus@gmail.com>
2019-01-16 23:02:04 -0500

    Bumping the version number to 1.1.1.

--- PyISY/__init__.py
+++ PyISY/__init__.py
@@ -23,7 +23,7 @@ limitations under the License.
 
 from .ISY import ISY
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 __author__ = 'Ryan M. Kraus'
 __email__ = 'rmkraus at gmail dot com'
 __date__ = 'December 2017'
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyISY',
-    version='1.1.0',
+    version='1.1.1',
     license='Apache License 2.0',
     url='http://automic.us/projects/pyisy',
-    download_url='https://github.com/automicus/pyisy/tarball/1.1.0',
+    download_url='https://github.com/automicus/pyisy/tarball/1.1.1',
     author='Ryan Kraus',
     author_email='automicus@gmail.com',
     description='Python module to talk to ISY994 from UDI.',
