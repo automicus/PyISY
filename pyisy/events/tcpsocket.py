a3cbcc92a704e7ee76db4d2a8265b7c217bc243b
shbatm <support@shbatm.com>
2023-03-21 07:20:43 -0500

    Handle unsub message with no stream id (#387)

--- pyisy/events/tcpsocket.py
+++ pyisy/events/tcpsocket.py
@@ -211,10 +211,10 @@ class EventStream:
     def unsubscribe(self):
         """Unsubscribe from the Event Stream."""
         if self._subscribed and self._connected:
-            msg = self._create_message(strings.UNSUB_MSG)
             try:
+                msg = self._create_message(strings.UNSUB_MSG)
                 self.write(msg)
-            except OSError as ex:
+            except (OSError, KeyError) as ex:
                 _LOGGER.error(
                     "PyISY encountered a socket error while writing unsubscribe message to the socket: %s.",
                     ex,
